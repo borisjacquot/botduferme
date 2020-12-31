9e755cfbe5fcae211cb8ed91ec89ec4ad626dab8
Boris Jacquot <boris.jacquot0@gmail.com>
2020-12-31 12:55:15 +0100

    fix(index.js): date didn't work

--- index.js
+++ index.js
@@ -13,17 +13,16 @@ const config = {
 
 const T = new Twit(config);
 
-let date_ob = new Date(),
-    date = ("0" + date_ob.getDate()).slice(-2),
-    month = ("0" + (date_ob.getMonth() + 1)).slice(-2),
-    year = date_ob.getFullYear();
-
-
 let j = schedule.scheduleJob('15 15 * * *', function(){
     T.get('statuses/user_timeline', { user_id: 'prisduferme', count: 1 }, function ( err, data, response ){
         let words = data[0].text.split(" ");
         let sam = words[3] - 1,
             moussa = words[12] - 1;
+
+        let date_ob = new Date(),
+            date = ("0" + date_ob.getDate()).slice(-2),
+            month = ("0" + (date_ob.getMonth() + 1)).slice(-2),
+            year = date_ob.getFullYear();
 
         let stat = date + '/' + month + '/' + year + ': Il reste ' + sam + ' jours de ferme à Sam et Redouane, et ' + moussa + ' jours à Moussa !';
 
